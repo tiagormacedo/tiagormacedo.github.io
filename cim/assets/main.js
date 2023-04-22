1ee9595bc147841f2712614b6ce09b028456de19
tiagormacedo <61014657+tiagormacedo@users.noreply.github.com>
2023-04-22 10:50:05 -0300

    Update main.js

--- cim/assets/main.js
+++ cim/assets/main.js
@@ -1,3 +1,35 @@
+// Swap the active (darker) side bar item
+function swapActiveIndicator(item) {
+    var indicators = document.getElementsByClassName("indicator");
+    for (ind of indicators) {
+        ind.classList.remove("active");
+    }
+    item.classList.add("active");
+}
+
+// Highlight the sidemenu item corresponding to the section that I'm reading
+function sidemenuHighlight() {
+    // Function that detects which section I am reading
+    function ioCallBack (entries) {
+        for (entry of entries) {
+            if (entry.isIntersecting) {
+                var link = "#" + entry.target.id;
+                var indicator = document.querySelector('[href*="' + link + '"]');
+                swapActiveIndicator(indicator);
+            }
+        }
+    }
+    let ioOptions = {root: null, rootMargin: "0px", threshold: 0.33};
+    let observer = new IntersectionObserver(ioCallBack, ioOptions);
+
+    // Apply the function above to each section
+    let sections = document.querySelectorAll("section");
+    for (section of sections) {
+        observer.observe(section);
+    }
+}
+
+// Function to process a tex file via a form
 function displayLink() {
     var baseurl = "https://tmacedo.pythonanywhere.com/uploads/";
     var infilename = document.querySelector('input[type=file]').files[0].name;
