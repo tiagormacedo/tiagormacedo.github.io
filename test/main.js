aea5dd75f1bd53cbc93fd5751d79ebafc2121f67
Tiago Macedo <61014657+tiagormacedo@users.noreply.github.com>
2024-01-02 11:13:08 -0300

    Create main.js

---
+++ test/main.js
@@ -0,0 +1,12 @@
+// Function to process a tex file via a form
+function displayLink() {
+    var baseurl = "https://tmacedo.pythonanywhere.com/uploads/";
+    var infilename = document.querySelector('input[type=file]').files[0].name;
+    infilename = infilename.replace(/ /g, "_");
+
+    var infilepath = baseurl + infilename;
+    document.getElementById("raw_outfile").innerHTML = "<a href=" + infilepath + " target='_blank'> raw file </a>";
+
+    var edited_filepath = infilepath.slice(0, -4) + "-edited" + infilepath.slice(-4);
+    document.getElementById("edited_outfile").innerHTML = "<a href="+edited_filepath+" download> edited file </a>";
+    }
