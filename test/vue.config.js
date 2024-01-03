9e0e689ee9d8953e8eab65ba1af0a9c937bc10eb
Tiago Macedo <61014657+tiagormacedo@users.noreply.github.com>
2024-01-03 17:42:41 -0300

    Update vue.config.js

--- test/vue.config.js
+++ test/vue.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/test/" : "/",
 };
+
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
